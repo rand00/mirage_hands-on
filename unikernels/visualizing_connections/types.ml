57314251c0e4b73f3de0ff802eab22685bdd2a92
rand00 <oth.rand@gmail.com>
2017-10-29 20:16:55 +0100

    Types: Added actor type to use in state.

--- unikernels/visualizing_connections/types.ml
+++ unikernels/visualizing_connections/types.ml
@@ -3,10 +3,12 @@ open Sexplib.Std
 (*Note that the cmd-type is specified as polymorphic variants
   in the Parse module *)
 
-type remote_msg = {
+type actor = {
   name : string;
   position : float;
 } [@@deriving sexp]
+
+type remote_msg = actor [@@deriving sexp]
 
 type master_msg = {
   name : string;
