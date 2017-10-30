847331a5c9874466673a93fcbb81a36c3ddcf380
rand00 <oth.rand@gmail.com>
2017-10-30 17:59:06 +0100

    Added 'master-page'

--- unikernels/visualizing_connections/masterpage.ml
+++ unikernels/visualizing_connections/masterpage.ml
@@ -1,20 +1,22 @@
+
 open Tyxml_html
 
 let histories_title =
-  title (pcdata "Visualizing connections between unikernels.")
+  title (pcdata "Master page.")
 
 let vector_history_box = div ~a:[a_id "vg_area"] []
 
 (*goto parametrize with inline js-string from vg or?*)
-let content ~ip_str =
+let content =
   html
     (head histories_title [])
     (body [
-        h1 [ pcdata @@ "Unikernel ip: "^ip_str ];
+        h1 [ pcdata "Master page" ];
         vector_history_box;
       ])
 
 let to_string content =
   Format.asprintf "%a" (Tyxml.Html.pp ()) content
+
 
 
